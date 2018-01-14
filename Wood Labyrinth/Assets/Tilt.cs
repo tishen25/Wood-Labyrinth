ad2d372b188846dc6423d14938ef97b43ffc3a0c
MegJ <meg.j.rock@gmail.com>
2018-01-14 14:59:47 -0500

    Added everything back

---
+++ Wood Labyrinth/Assets/Tilt.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilt : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float tiltHorizontal = Input.GetAxis("Horizontal");
+        float tiltVertical = Input.GetAxis("Vertical");
+
+		print (transform.localRotation.z);
+		if(transform.localRotation.x > .05 && tiltVertical > 0)
+		{
+			tiltVertical = 0;
+		}
+
+		if (transform.localRotation.x < -.05 && tiltVertical < 0) 
+		{
+			tiltVertical = 0;
+		}
+
+		if(transform.localRotation.z > .05 && tiltHorizontal < 0)
+		{
+			tiltHorizontal = 0;
+
+		}
+		if (transform.localRotation.z < -.05 && tiltHorizontal > 0) {
+			tiltHorizontal = 0;
+		}
+
+        transform.Rotate(new Vector3(tiltVertical, 0, -tiltHorizontal));
+
+    }
+}
